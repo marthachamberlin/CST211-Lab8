f19e69ae7c76ce9ab29596650581377ff0ae3047
MARTHA J CHAMBERLIN <martha.chamberlin@oit.edu>
2021-12-28 13:04:24 -0800

    Initial commit

---
+++ BlankConsoleLab/BlankConsoleLab.cpp
@@ -0,0 +1,16 @@
+// BlankConsoleLab.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
